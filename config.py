888d602b92ac8e909eff972fdfd1e4084c1f67c9
Jian Wu <jian.wu@nio.io>
2018-11-02 16:35:33 -0700

    setup env for Redis

--- config.py
+++ config.py
@@ -2,8 +2,18 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
+local_env = os.path.join(basedir, '.env')
+
+if os.path.exists(local_env):
+    print(f'loading local environment variables from {local_env}')
+    load_dotenv(os.path.join(basedir, '.env'))
+else:
+    print("running inside docker ...")
 
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "flask-rq-dashboard-bootstrap4"
+    REDIS_HOST = os.environ.get('REDIS_HOST') or "localhost"
+    REDIS_PORT = os.environ.get('REDIS_PORT') or "6379"
+    REDIS_DB = os.environ.get('REDIS_DB') or "0"
+    RQ_POLL_INTERVAL = os.environ.get('RQ_POLL_INTERVAL') or "2500"
