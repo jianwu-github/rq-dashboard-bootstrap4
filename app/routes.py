0dba58bcd6138c0496f157bb6c45d6a11687b001
Jian Wu <hellojianwu@gmail.com>
2018-11-03 17:25:27 -0700

    create UI pages and navigation

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,28 @@ from flask import render_template, redirect, url_for
 
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/queues')
+def queues():
+    return render_template('queues.html')
+
+
+@app.route('/workers')
+def workers():
+    return render_template('workers.html')
+
+
+@app.route('/jobs')
+def jobs():
+    return render_template('jobs.html')
+
+
+@app.route('/schedulers')
+def schedulers():
+    return render_template('schedulers.html')
